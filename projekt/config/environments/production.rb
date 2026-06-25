2e6c7b054624699ecfa53ee1407b76cde1c9765b
krixzy <mortensenkim@msn.com>
2026-06-25 21:37:31 +0200

    plz fix

--- projekt/config/environments/production.rb
+++ projekt/config/environments/production.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.hosts << "lightarti.mrruby.dk"
+  config.hosts << "host.docker.internal"
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
